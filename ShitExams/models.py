f92d3bee58897fb3e58acdbae53a983e55047d43
Hrafnkell Brimar Hallmundsson <hrafnkell@hrafnkell-laptop.(none)>
2012-02-10 23:53:12 +0000

    ShitExams

---
+++ ShitExams/models.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+
+class Exam(models.Model):
+    name = models.CharField(max_length=64)
+    deadline = models.DateTimeField(default=datetime.now())
+    author = models.CharField(max_length=64)
