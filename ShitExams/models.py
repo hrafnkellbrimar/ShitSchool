1cf9a02bb78af3fe7ccd15bbe9be9d9611a0848f
Hrafnkell Brimar Hallmundsson <hrafnkell@hrafnkell-laptop.(none)>
2012-02-13 12:09:30 +0000

    more cleaning

--- ShitExams/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from django.db import models
-
-# Create your models here.
-
-class Exam(models.Model):
-    name = models.CharField(max_length=64)
-    deadline = models.DateTimeField(default=datetime.now())
-    author = models.CharField(max_length=64)
--- ShitSchool/django_auth_ldap/__init__.py
+++
@@ -1,2 +0,0 @@
-version = (1, 0, 15)
-version_string = "1.0.15"
